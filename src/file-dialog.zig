72bbca68c4c0823131352e464613e51f447d5e96
Jordan Baron <jordannbaron@gmail.com>
2024-07-16 17:58:38 -0400

    file save dialog

---
+++ src/file-dialog.zig
@@ -0,0 +1,18 @@
+// binding ripped from https://github.com/fabioarnold/nfd-zig/blob/master/src/lib.zig
+const std = @import("std");
+const c = @cImport({
+    @cInclude("nfd.h");
+    @cInclude("stdlib.h");
+});
+
+pub fn openFileDialog(filter: ?[:0]const u8, default_path: ?[:0]const u8) !?[:0]const u8 {
+    var out_path: [*c]u8 = null;
+
+    const result = c.NFD_SaveDialog(if (filter != null) filter.? else null, if (default_path != null) default_path.? else null, &out_path);
+
+    return switch (result) {
+        c.NFD_OKAY => if (out_path == null) null else std.mem.sliceTo(out_path, 0),
+        c.NFD_ERROR => return error.NFDError,
+        else => null,
+    };
+}
