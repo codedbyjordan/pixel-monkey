f9c64f1ad231cc4064dee000e63b4c7e2f8c4b1c
Jordan Baron <jordannbaron@gmail.com>
2024-07-16 23:32:36 -0400

    initialize array with loop over mem.zeros (faster)

--- src/main.zig
+++ src/main.zig
@@ -17,7 +17,12 @@ pub fn main() anyerror!void {
     const screenHeight = 450;
 
     var colorPickerColor = rl.Color.red;
-    var pixels = std.mem.zeroes([gui.gridWidth * gui.gridHeight]rl.Color);
+    var pixels: [gui.gridWidth * gui.gridHeight]rl.Color = undefined;
+
+    var i: usize = 0;
+    while (i < gui.gridWidth * gui.gridHeight) : (i += 1) {
+        pixels[i] = emptyColor;
+    }
 
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
     defer rl.closeWindow();
